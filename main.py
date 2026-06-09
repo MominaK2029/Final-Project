63e4084bcbffbdb5b4d562c7add19253f4c021e9
MominaK2029 <student.165557@worcesterschools.net>
2026-06-09 11:34:43 -0400

    Ongoing

--- main.py
+++ main.py
@@ -1,32 +1,53 @@
-import streamlit as st
 import pandas as pd
-st.title("Work Organization")
+import streamlit as st
+tab1, tab2, tab3 = st.tabs(["Tasks", "Shop", "Manage"])
+with tab1:
+    st.title("Work Organization")
 
-hardtask = st.data_editor({})
-minimaltask = st.data_editor({})
-easytask = st.data_editor({})
+    hardtask = st.data_editor
+    minimaltask = st.data_editor
+    easytask = st.data_editor
 
-hardtask = pd.DataFrame({
-    'Hard Task':["Clean the house"]
-})
-minimaltask = pd.DataFrame({
-    'Minimal Task':["Broom the house"]
-})
-easytask = pd.DataFrame({
-    'Easy Task':["Lock the door"]
-})
+    hardtask = pd.DataFrame({
+        'Hard Task':["Clean the house"],
+        'Coins':["14"]
+    })
+    minimaltask = pd.DataFrame({
+        'Minimal Task':["Broom the house"],
+        'Coins':["8"]
+    })
+    easytask = pd.DataFrame({
+        'Easy Task':["Lock the door"],
+        'Coins':["4"]
+    })
 
-search_query1 = st.text_input("Search Type", placeholder="Type here... ex: Hard")
+    search_query1 = st.text_input("Search Type of Task", placeholder="Type here... ex: Minimal")
 
-if search_query1.strip().lower() == "Easy":
-    st.write(f"Results for '{search_query1}':")
-    st.dataframe(easytask, hide_index=True)
-elif search_query1.strip().lower() == "Minimal":
-    st.write(f"Results for '{search_query1}':")
-    st.dataframe(easytask, hide_index=True)
-elif search_query1.strip().lower() == "Hard":
-    st.write(f"Results for '{search_query1}':")
-    st.dataframe(easytask, hide_index=True)
-else:
-    st.write("Showing all records:")
-    st.dataframe(easytask, hide_index=True)
+    if search_query1.strip().lower() == "Easy":
+        st.write(f"Results for '{search_query1}':")
+        st.dataframe(easytask, hide_index=True)
+        filtered_easy = easytask[
+            easytask['Easy Task'].str.contains(search_query1, case=False) 
+            ]
+        st.write(f"Results for '{search_query1}':")
+        st.dataframe(filtered_easy, hide_index=True)
+    elif search_query1.strip().lower() == "Minimal":
+        st.write(f"Results for '{search_query1}':")
+        st.dataframe(hardtask, hide_index=True)
+    elif search_query1.strip().lower() == "Hard":
+        st.write(f"Results for '{search_query1}':")
+        st.dataframe(hardtask, hide_index=True)
+    else:
+        st.write("Showing all records:")
+        st.dataframe(easytask, hide_index=True)
+        st.dataframe(minimaltask, hide_index=True)
+        st.dataframe(hardtask, hide_index=True)
+with tab3:
+    task = st.menu_button("Section..", options=["Easy", "Minimal", "Hard"])
+    if task.strip().lower() == "Easy":
+        search = st.text_input("Type in your task")
+    elif task.strip().lower() == "Minimal":
+        search = st.text_input("Type in your task")
+    else:
+        task.strip().lower() == "Minimal"
+        search = st.text_input("Type in your task") 
