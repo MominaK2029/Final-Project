85f9e1721591cb7a950811a7d7c08d7bb0802019
Momina Khan <mominak@webdev18.local>
2026-06-11 13:36:56 -0400

    Test Subject

--- main2.py
+++ main2.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import pandas as pd
+tab1, tab2 = st.tabs(["Tasks", "Manage"])
+with tab1:
+    st.title("Work Organization")
+    if 'df' not in st.session_state:
+        st.session_state.df = pd.DataFrame({"Tasks": ["Type a task",], "Reward": [25, 30]})
 
+# 2. Assign the editor to a variable or handle edits using a callback
+    edited_df = st.data_editor(st.session_state.df, hide_index=True, num_rows="dynamic")
+
+# 3. Use the updated variable for the rest of your app
+    st.write("'Fall seven times, stand up eight.' — Japanese Proverb")
